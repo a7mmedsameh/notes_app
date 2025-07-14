8d250da9c0efa83ac78c2a7ac5631af5aff1ef46
a7mmedsameh <elmandoooooo1@gmail.com>
2025-07-14 07:54:06 +0300

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,26 @@ import 'package:project_12/views/notes_view.dart';
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-            designSize: const Size(360, 690),
+      designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-         builder: (context , child) { 
-          return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: const NotesView(),
-      );
-         }
-    
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: 'Poppins'
+          ),
+          home: const NotesView(),
+        );
+      },
     );
   }
 }
---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.r),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.all(0),
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26.sp),
+            ),
+            subtitle: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.r),
+              child: Text(
+                'Build your career with Ahmed Sameh',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24.sp,
+              ),
+            ),
+          ),
+          Text(
+            'May21, 2022',
+            style: TextStyle(color: Colors.black.withOpacity(.5)),
+          ),
+        ],
+      ),
+    );
+  }
+}
