e053251b6ae5764e114fc277a3c47a82822cd920
a7mmedsameh <elmandoooooo1@gmail.com>
2025-07-15 09:52:59 +0300

    add hive package

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -6,6 +6,6 @@ class EditNoteView extends StatelessWidget {
   static String id = 'EditNoteView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: EditNoteViewBody());
+    return const Scaffold(body: EditNoteViewBody());
   }
 }
