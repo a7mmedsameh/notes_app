8afc185c1c0ed3a072c5a1cecf3dc662c407faa7
a7mmedsameh <elmandoooooo1@gmail.com>
2025-07-16 12:24:09 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:project_12/constatns.dart';
+import 'package:project_12/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:project_12/models/note_model.dart';
 import 'package:project_12/views/edit_note_view.dart';
 import 'package:project_12/views/notes_view.dart';
@@ -23,14 +25,20 @@ class NotesApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-          routes: {
-            EditNoteView.id: (context) => const EditNoteView(),
-            NotesView.id: (context) => const NotesView(),
-          },
-          initialRoute: NotesView.id,
+        return MultiBlocProvider(
+          providers: [BlocProvider(create: (context) => NotesCubit())],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              brightness: Brightness.dark,
+              fontFamily: 'Poppins',
+            ),
+            routes: {
+              EditNoteView.id: (context) => const EditNoteView(),
+              NotesView.id: (context) => const NotesView(),
+            },
+            initialRoute: NotesView.id,
+          ),
         );
       },
     );
